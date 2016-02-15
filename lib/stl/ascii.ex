95d5fc20d31caf068510140f08e4c674ea280e1d
Marco Spatz <marco.spatz@googlemail.com>
2016-02-15 22:27:29 +0100

    implemented parsing the stl ascii lines

--- lib/stl/ascii.ex
+++ lib/stl/ascii.ex
@@ -1,5 +1,47 @@
 defmodule FileFormats3D.Stl.Ascii do
-  def read (ascii_stl_content) do
 
+  def read (ascii_stl_content) do
+    ascii_stl_content
+    |> Stream.map(&String.rstrip/1)
+    |> Stream.map(&String.split(&1, " ", trim: true))
+    |> Stream.map(&parse/1)
+    |> Enum.to_list
+  end
+
+  def parse(["solid"|tail]) do
+    {:solid , Enum.join(tail, " ")}
+  end
+
+  def parse(["endsolid"|tail]) do
+    {:end_solid , Enum.join(tail, " ")}
+  end
+
+  def parse(["facet"|tail]) do
+    {:facet, parse_vector("normal", tail)}
+  end
+
+  def parse (["endfacet"]) do
+    {:end_facet, nil}
+  end
+
+  def parse (["outer", "loop"]) do
+    {:outer_loop, nil}
+  end
+
+  def parse (["endloop"]) do
+    {:end_loop, nil}
+  end
+
+  def parse(["vertex"|_] = tokens) do
+    parse_vector("vertex", tokens)
+  end
+
+  def parse(tokens) do
+    line = Enum.join(tokens, " ")
+    {:error, "Could not parse line \"#{line}\""}
+  end
+
+  def parse_vector(key, [key, x, y, z]) do
+    {String.to_atom(key), [String.to_float(x), String.to_float(y), String.to_float(z)]}
   end
 end
--- test/stl/ascii_test.exs
+++ test/stl/ascii_test.exs
@@ -5,7 +5,38 @@ defmodule FileFormats3DTest.Stl.Ascii do
   alias FileFormats3D.Stl.Ascii
 
   test "read" do
-    {:ok, ascii_cube_content} = File.read("test/_data/stl/cube_ascii.stl")
-    assert [] == Ascii.read(ascii_cube_content)
+    assert 86 == Enum.count(Ascii.read(File.stream!("test/_data/stl/cube_ascii.stl")))
+  end
+
+  test "parse solid line" do
+    assert {:solid, "Exported from Blender-2.76 (sub 0)"} = Ascii.parse(["solid", "Exported", "from", "Blender-2.76", "(sub", "0)"])
+  end
+
+  test "parse endsolid line" do
+    assert {:end_solid, "Exported from Blender-2.76 (sub 0)"} = Ascii.parse(["endsolid", "Exported", "from", "Blender-2.76", "(sub", "0)"])
+  end
+
+  test "parse facet line" do
+    assert {:facet, {:normal, [-0.0, 0.0, -1.0]}} = Ascii.parse(["facet", "normal", "-0.000000", "0.000000", "-1.000000"])
+  end
+
+  test "parse endfacet line" do
+    assert {:end_facet, _} = Ascii.parse(["endfacet"])
+  end
+
+  test "parse outer loop" do
+    assert {:outer_loop, _} = Ascii.parse(["outer", "loop"])
+  end
+
+  test "parse endloop line" do
+    assert {:end_loop, _} = Ascii.parse(["endloop"])
+  end
+
+  test "parse vertex line" do
+    assert {:vertex, [1.0, 1.0, -1.0]}= Ascii.parse(["vertex", "1.000000", "1.000000", "-1.000000"])
+  end
+
+  test "parse vertex with exp coords" do
+    assert {:vertex, [-3.1533, 6.664, -12.4451]}= Ascii.parse(["vertex", "-0.315330E+01",  "0.666400E+01", "-0.124451E+02"])
   end
 end
