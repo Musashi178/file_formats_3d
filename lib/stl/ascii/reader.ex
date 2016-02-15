fd522e008afd8e7ce763df66e39824c53a6b4c2f
Marco Spatz <marco.spatz@googlemail.com>
2016-02-15 23:16:24 +0100

    implemented ascii file parsing

--- lib/stl/ascii/reader.ex
+++ lib/stl/ascii/reader.ex
@@ -2,10 +2,44 @@ defmodule FileFormats3D.Stl.Ascii.Reader do
 
   def read (ascii_stl_content) do
     ascii_stl_content
-    |> Stream.map(&String.rstrip/1)
-    |> Stream.map(&String.split(&1, " ", trim: true))
-    |> Stream.map(&parse/1)
+    |> strip_newlines_at_end_of_lines
+    |> tokenize_lines
+    |> parse_lines
+    |> remove_unneeded_parsed_lines
+    |> extract_faces
     |> Enum.to_list
+  end
+
+  defp strip_newlines_at_end_of_lines(lines), do: Stream.map(lines, &String.rstrip/1)
+
+  defp tokenize_lines(lines), do: Stream.map(lines, &String.split(&1, " ", trim: true))
+
+  defp parse_lines(lines), do: Stream.map(lines, &parse/1)
+
+  def remove_unneeded_parsed_lines(lines) do
+    lines |> Stream.filter(&is_required_line?/1)
+
+  end
+
+  def is_required_line?(parsed_line) do
+    case elem(parsed_line, 0) do
+      :solid -> false
+      :end_solid -> false
+      :outer_loop -> false
+      :end_loop -> false
+      :end_facet -> false
+      _ -> true
+    end
+  end
+
+  def extract_faces(parsed_lines) do
+    parsed_lines
+    |> Stream.chunk(4)
+    |> Stream.map(&extract_face/1)
+  end
+
+  def extract_face([facet: {:normal, normal}, vertex: v0, vertex: v1, vertex: v2]) do
+    %{normal: normal, v0: v0, v1: v1, v2: v2 }
   end
 
   def parse(["solid"|tail]) do
